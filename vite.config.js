b3d2201dd00a72af0bb44da53fdcd3df03161629
shuvomdhar <shuvomdhar8@gmail.com>
2025-09-26 22:02:01 +0530

    Added Chess Game

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    allowedHosts: ["chess-game-m24m.onrender.com"]
+  }
 })
